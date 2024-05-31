08ab9e257d7c3de188559681484a019e04015091
andrastoth1980 <andrastoth210@gmail.com>
2024-05-31 18:56:05 +0200

    database connection tested

--- src/main/java/com/globalsoftwaresupport/views/MainView.java
+++ src/main/java/com/globalsoftwaresupport/views/MainView.java
@@ -7,6 +7,8 @@ import com.globalsoftwaresupport.model.Status;
 import com.globalsoftwaresupport.model.Student;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -40,13 +42,32 @@ public class MainView extends VerticalLayout{
 		grid.setSizeFull();
 		grid.setColumns("country" , "zipCode");
 		grid.addColumn(s -> s.getName()).setHeader("Name");
-		
+		grid.addColumn(s -> s.getAge()).setHeader("Age");
+		grid.addComponentColumn(s -> {
+			Icon icon;
+			
+			if(s.getStatus().getName().equals("ACTIVE")) {
+				icon = VaadinIcon.CIRCLE.create();
+						icon.setColor("green");
+				} else if(s.getStatus().getName().equals("PASSIVE")) {
+					icon = VaadinIcon.CLOSE_CIRCLE.create();
+							icon.setColor("red");
+				} else {
+					icon = VaadinIcon.CHECK_CIRCLE.create();
+							icon.setColor("orange");
+				}
+			return icon;
+		}).setHeader("Status");
+		grid.getColumns().forEach(col -> col.setAutoWidth(true));
 		
 	}
 
 	private void loadStudents() {
 		List<Student> students = new ArrayList<>();
 		students.add(new Student("Adam",23,1627, "UK",new Status("ACTIVE")));
+		students.add(new Student("Kevin",23,1627, "UK",new Status("PASSIVE")));
+		students.add(new Student("Emily",23,1627, "UK",new Status("ABSOLVED")));
+		
 		grid.setItems(students);
 		
 		
