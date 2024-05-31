dc1d81aa1b76c641f0ee6431598ea646f97367a5
andrastoth1980 <andrastoth210@gmail.com>
2024-05-31 17:51:30 +0200

    added model+grid

---
+++ src/main/java/com/globalsoftwaresupport/model/Status.java
@@ -0,0 +1,24 @@
+package com.globalsoftwaresupport.model;
+
+public class Status {
+	
+
+	private String name;
+	
+	public Status(String name) {
+		this.name = name;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
--- src/main/java/com/globalsoftwaresupport/views/MainView.java
+++ src/main/java/com/globalsoftwaresupport/views/MainView.java
@@ -1,6 +1,12 @@
 package com.globalsoftwaresupport.views;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.globalsoftwaresupport.model.Status;
+import com.globalsoftwaresupport.model.Student;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -9,8 +15,19 @@ import com.vaadin.flow.router.Route;
 @Route(value = "")
 
 public class MainView extends VerticalLayout{
+	
+	private Grid<Student> grid;
 
 	public MainView() {
-		add(new Button("Click me!"));
+		
+		setSizeFull();
+		setAlignItems(Alignment.CENTER);
+		
+		List<Student> students = new ArrayList<>();
+		students.add(new Student("Adam",23,1627, "UK",new Status("ACTIVE")));
+		grid = new Grid<>(Student.class);
+		grid.setItems(students);
+		
+		add(grid);
 	}
 }
