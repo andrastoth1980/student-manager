d1db5d6de1ed985c31ba150c42e8226ec9ae160c
andrastoth1980 <andrastoth210@gmail.com>
2024-06-02 12:28:35 +0200

    Repository added

---
+++ src/main/java/com/globalsoftwaresupport/repository/StatusRepository.java
@@ -0,0 +1,5 @@
+package com.globalsoftwaresupport.repository;
+
+public class StatusRepository {
+
+}
---
+++ src/main/java/com/globalsoftwaresupport/repository/StudentRepository.java
@@ -0,0 +1,5 @@
+package com.globalsoftwaresupport.repository;
+
+public class StudentRepository {
+
+}
